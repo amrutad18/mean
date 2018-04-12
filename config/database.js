a88fb8b76d41da5abdb77f2c23319c8819104f46
amrutad18 <amrutadeshpande18@gmail.com>
2018-04-12 19:05:48 +0530

    Database URL changed to AWS mongodb instance

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 // config/database.js
 module.exports = {
 
-	'url' : 'mongodb://amruta:amruta@ds243059.mlab.com:43059/mean-sample' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+	'url' : 'mongodb://amrutalocal:amrutalocal@ec2-18-221-93-177.us-east-2.compute.amazonaws.com:27017/localDB' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
 
 };
