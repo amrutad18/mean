a5a540a2a5f40711d2320b873efabf23a4339ba1
amrutad18 <amrutadeshpande18@gmail.com>
2018-04-12 19:43:45 +0530

    commit

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 // config/database.js
 module.exports = {
 
-	'url' : 'mongodb://amrutalocal:amrutalocal@ec2-18-221-93-177.us-east-2.compute.amazonaws.com:27017/localDB' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+	'url' : 'mongodb://amrutalocal:amrutalocal@ec2-18-221-90-2.us-east-2.compute.amazonaws.com:27017/localDB' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
 
 };
